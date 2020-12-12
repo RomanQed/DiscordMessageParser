f12fad2d46456203461b96afa55358bc5878630b
Roman Bakaldin <gbakaldin@gmail.com>
2020-12-13 02:17:07 +0300

    Upd token

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -4,7 +4,7 @@ import javax.security.auth.login.LoginException;
 
 public class Bot {
     public static void main(String[] args) throws Exception {
-        String token = "NjkwMTgwMzc4MzQ2MTI3NDcy.XnNq4A.F08VZ7rqDxoByVTK9Cx0poGFq6c";
+        String token = "NjkwMTgwMzc4MzQ2MTI3NDcy.XnNqkQ.c4ZwxdJb-ZRz4jPD5eLc-mWW6pk";
         JDABuilder bot = JDABuilder.createDefault(token);
         bot.addEventListeners(new CustomListenerAdapter());
         bot.build();
